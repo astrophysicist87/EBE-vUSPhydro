f3d33231bc28626d06d22c479bf680c92607ec62
Christopher Plumberg <plumberg.1@osu.edu>
2021-04-23 16:00:50 -0500

    Update SPH.h

--- EBE-Node/df2/SPH.h
+++ EBE-Node/df2/SPH.h
@@ -1390,12 +1390,12 @@ void SPH<D,DD>::Iout(double &I1, double &I2, double p, double phi, HAD cur,
 		}
 	}
 	
-	cout << "Check real: " << I1 << "   " << I2 << "   "
+	/*cout << "Check real: " << I1 << "   " << I2 << "   "
 		<< I1c << "   " << I2c << "   "
 		<< I1sc << "   " << I2sc << "   "
 		<< ST_I1[0] << "   " << ST_I2[0] << "   "
 		<< ST_I1c[0] << "   " << ST_I2c[0] << "   "
-		<< ST_I1sc[0] << "   " << ST_I2sc[0] << endl;
+		<< ST_I1sc[0] << "   " << ST_I2sc[0] << endl;*/
 
 	/*cout << "Check OLD: " << I1 << "   " << I2 << "   "
 		<< I1c << "   " << I2c << "   "
